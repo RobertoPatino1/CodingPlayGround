c48988d5cdf3e33df6756b1a28701a4d5644ea8f
RobertoPatino1 <rapatino@espol.edu.ec>
2023-03-23 20:07:43 -0500

    Second push

--- Dart/DummyCode/hello.dart
+++ Dart/DummyCode/hello.dart
@@ -1,3 +1,20 @@
 void main() {
   print("Hello world!\nThis is my first Dart program");
+
+  //Variables
+  var firstName = "Ryan"; //Data types can be declared or infered
+  String lastName = "Gosling";
+  int age = 42;
+
+  //Lists can contain various data types!
+  var movies = ['Drive', 'Blade Runner 2049', 'Barbie'];
+
+  //
+  var score = {'Drive': 89, 'Blade Runner 2049': 97, 'Barbie': 100};
+
+  // for (int i = 0; i < movies.length; i++) {
+  //   print(movies[i]);
+  // }
+
+  print(score['Drive']);
 }
