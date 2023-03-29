8d70c57cbe6b7aae5830cf39076d56a4dccb044e
RobertoPatino1 <rapatino@espol.edu.ec>
2023-03-28 20:50:59 -0500

    Added sets

--- Dart/DummyCode/collections/lists.dart
+++ Dart/DummyCode/collections/lists.dart
@@ -5,4 +5,11 @@ void main() {
   var list3 = [
     ...list1
   ]; //This addes only the elements of list1 to list3  (no inner list)
+
+  //Testing null awareness
+  var list4 = null;
+  var list5 = [
+    ...?list4
+  ]; //Null awareness '?' prevents the code from running into exceptions
+  print(list5); //Null element is not added to the list
 }
---
+++ Dart/DummyCode/collections/sets.dart
@@ -0,0 +1,8 @@
+void main() {
+  var set1 = {'Apples', "Bananas", "Mangoes"};
+
+  //Iterating
+  for (var fruit in set1) {
+    print(fruit);
+  }
+}
