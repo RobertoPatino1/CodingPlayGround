ecb47b520ac94b48565fca0ea0ad1025328326d6
RobertoPatino1 <93289504+RobertoPatino1@users.noreply.github.com>
2023-05-29 22:40:15 -0500

    Delete Dart/DummyCode directory

--- Dart/DummyCode/collections/maps.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var countries = ['USA', 'SP', 'JAP'];
-  var map = new Map<int, String>(); //"new" keyword is optional
-  for (int i = 1; i <= countries.length; i++) {
-    map[i] = countries[i - 1];
-  }
-  print(map);
-}
--- Dart/DummyCode/collections/sets.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var set1 = {'Apples', "Bananas", "Mangoes"};
-
-  //Iterating
-  for (var fruit in set1) {
-    print(fruit);
-  }
-}
