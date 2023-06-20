68951c03a8763e9baedb11c6e85b770241f67bba
RobertoPatino1 <rapatino@espol.edu.ec>
2023-06-20 01:06:02 -0500

    Added main.js

---
+++ src/JavaScript/main.js
@@ -0,0 +1,11 @@
+let firstName = "Roberto"
+let age = 21;
+let names = ["Jamie","Lucas","Roberto"]
+
+for(let i = 0;i<names.length;i++){
+    if(names[i]==firstName){
+        console.log("Success")
+    }else{
+        console.log("Not");
+    }
+}
