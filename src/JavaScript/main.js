ae8bc8963d5643d55088dbe41ba9a86ee7fa9e27
RobertoPatino1 <rapatino@espol.edu.ec>
2023-06-26 12:07:55 -0500

    Added js changes

--- src/JavaScript/main.js
+++ src/JavaScript/main.js
@@ -1,11 +1,61 @@
-let firstName = "Roberto"
-let age = 21;
-let names = ["Jamie","Lucas","Roberto"]
+// let a,b;
+// a = 12;
+// b = 3;
 
-for(let i = 0;i<names.length;i++){
-    if(names[i]==firstName){
-        console.log("Success")
-    }else{
-        console.log("Not");
-    }
+
+// const PI = 3.14;    //Constant values cannot be changed!!!
+// let result = a*b;   //This is not a constant, so it can be changed
+// console.log(result)
+// console.log(PI)
+
+
+/*
+Data types
+String, number, bigint, boolean, undefined, null, symbol, object (object,array,date)
+*/
+// console.log(typeof PI)
+
+
+
+// document.getElementById("mainTittle").innerHTML = "Hello Dolly.";
+// console.log("Hi")
+
+
+
+
+/*
+Functions!!!!
+*/
+
+function salute(name){
+    console.log("Hello "+name+"!!!")
 }
+function saluteWithReturn(name){
+    return "Hello "+name+"!!!";
+}
+
+salute("Roberto")
+
+let saluteVar = saluteWithReturn("Roberto")
+console.log(saluteVar)
+
+
+
+/*
+Functions can be used the same way as variables
+*/
+
+
+/*
+----------OBJECTS----------
+- You can use the "this" keyword the same way as you use it in Java
+- To access object properties declared as string you need to use this syntax: objectName['propertyName']
+*/
+const person = {
+    firstName: "Roberto",       //Para acceder a cualquiera de los campos solo colocamos person.nombreCampo
+    lastName :"Patino",
+    age:21
+}
+
+
+console.log(person)
---
+++ src/JavaScript/objects.js
@@ -0,0 +1,7 @@
+const person = {
+    firstName: "Roberto",
+    lastName :"Patino",
+    age:21
+}
+
+console.log(person)
